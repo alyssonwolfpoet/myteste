cceafddfa572c861b9c172cd40d4f55437c829df
Alysson <alyssos@hotmail.com>
2024-08-12 17:00:34 -0300

    feat: try fix

--- clients/ollama_client.py
+++ clients/ollama_client.py
@@ -9,16 +9,16 @@ from dotenv import load_dotenv
 load_dotenv()  # Carrega variáveis do arquivo .env
 
 #ollama = Ollama(api_key=os.getenv("OLLAMA_API_KEY"))
-embedding = FastEmbedEmbeddings()
+#embedding = OllamaEmbeddings()
 
 #ollama = Ollama()
 
+embedding = OllamaEmbeddings(model="llama3")
 
-
-embeddings = OllamaEmbeddings(model="llama3")
 def generate_embedding(text: str) -> list:
-    response = ollama.embed(input==text,model=embeddings)
-    return response['embedding']
+    response = embedding.embed_query(text)
+    print(response)
+    return response
 
 def ask_question(question: str, context: str) -> str:
     response = ollama.complete(prompt=f"Context: {context}\n\nQuestion: {question}", model="llama3.1")
